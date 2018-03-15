89f55421422be3c668d5682049c3f407b208a85e
lupakivan <lupakivan92@gmail.com>
2018-03-15 18:39:21 +0200

    If name == main

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
